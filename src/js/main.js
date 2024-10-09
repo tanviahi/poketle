148aa1bc174c102eb355577b855426cf51d086c1
Tanviahire22 <147433984+tanviahi@users.noreply.github.com>
2024-10-09 14:41:14 +0000

    done js

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,19 @@
 import data from "./data.json";
 
-for (let object of data){
-console.log(object.name);
-}
+// for (let object of data){
+// console.log(object.name);
+// }
  
 // for (let name of data){
 //     console.log(data);
 //     }
+
+const inputEl = document.querySelector("#floatingInputGroup1")
+console.log(inputEl)
+
+document.addEventListener("keyup", (input) => {
+    if (input.key === "/"){ 
+    inputEl.focus();
+    }
+});
+
