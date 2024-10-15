9d4cd0fa997fae87a79e240e8b690791b857b6fe
Tanviahire22 <147433984+tanviahi@users.noreply.github.com>
2024-10-15 13:00:01 +0000

    dist ins

--- README.md
+++ README.md
@@ -3,3 +3,11 @@ Bootstrap
 
 
 load json file to js using import 
+
+
+npm run dev (run through build tool like vite)
+hml = hot module replacement
+
+
+
+npm run build create dist floder 
--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,8 @@ import { defineConfig } from "vite";
 export default defineConfig({
     root:"src",
     publicDir:"../public",
+    build: {
+        outDir: "../dist",
+        emptyOutDir:true,
+    },
 });
